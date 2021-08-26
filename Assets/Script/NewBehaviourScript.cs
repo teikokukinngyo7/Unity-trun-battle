45570493f97e13d9ed676db6f24b6e5a85c3c527
teikokukinngyo7 <teikokukinngyo7@gmail.com>
2021-08-26 22:54:41 +0900

    no message

--- Assets/Script/NewBehaviourScript.cs
+++ Assets/Script/NewBehaviourScript.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//test
+//test testdao
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
