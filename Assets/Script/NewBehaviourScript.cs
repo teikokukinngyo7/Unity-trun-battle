26d3c5eb72c25a3c9574b1ac242405eaf149beae
teikokukinngyo7 <teikokukinngyo7@gmail.com>
2021-08-26 22:54:12 +0900

    test

--- Assets/Script/NewBehaviourScript.cs
+++ Assets/Script/NewBehaviourScript.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//test
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
