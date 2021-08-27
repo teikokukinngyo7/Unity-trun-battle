d2fed7387126b2b8db48dde35128567ef659d8c6
teikokukinngyo7 <teikokukinngyo7@gmail.com>
2021-08-27 20:11:49 +0900

    test

--- Assets/Script/BattleManagerCS.cs
+++ Assets/Script/BattleManagerCS.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class NewBehaviourScript : MonoBehaviour
+//戦いを管理
+public class BattleManagerCS : MonoBehaviour
 {
-    // Start is called before the first frame updatetest2fesf
+    // Start is called before the first frame update
     void Start()
     {
-        //testdevelop
+        
     }
 
     // Update is called once per frame
--- Assets/Script/UnitManagerCS.cs
+++ Assets/Script/UnitManagerCS.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//test testdao
-public class NewBehaviourScript : MonoBehaviour
+//キャラクターを管理する。
+public class UnitManagerCS : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
